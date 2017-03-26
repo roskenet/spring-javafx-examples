573283e5fd344a3d81ac8ec23ed00a37bad29e7b
Felix Roske <f.roske@roskenet.de>
2017-03-26 22:15:41 +0200

    Call showView static

--- part_6_2/src/main/java/example/ViewController.java
+++ part_6_2/src/main/java/example/ViewController.java
@@ -1,21 +1,16 @@
 package example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import de.felixroske.jfxsupport.FXMLController;
 import javafx.event.Event;
 
 @FXMLController
 public class ViewController {
     
-    @Autowired
-    private Main main;
-    
     public void showFirstView(Event event) {
-        main.showView(FirstView.class);
+        Main.showView(FirstView.class);
     }
 
     public void showSecondView(Event event) {
-        main.showView(SecondView.class);
+        Main.showView(SecondView.class);
     }
 }
