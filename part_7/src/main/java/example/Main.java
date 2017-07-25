879d20f873dbe1cf1205b208e052219c5635a1ee
Felix Roske <f.roske@roskenet.de>
2017-07-25 10:03:32 +0200

    Add part 7

---
+++ part_7/src/main/java/example/Main.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
+
+@SpringBootApplication
+public class Main extends AbstractJavaFxApplicationSupport{
+    public static void main(String[] args) {
+        launchApp(Main.class, HelloworldView.class, args);
+    }
+}
