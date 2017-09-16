f21eaa6e551f355bc235efda14a8e2c4902474a9
Felix Roske <f.roske@roskenet.de>
2017-09-16 10:37:08 +0200

    Draggable Helloworld

---
+++ part_1/src/main/java/example/HelloworldController.java
@@ -0,0 +1,27 @@
+package example;
+
+import de.felixroske.jfxsupport.FXMLController;
+import de.felixroske.jfxsupport.GUIState;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+@FXMLController
+public class HelloworldController {
+    private double xOffset = 0;
+    private double yOffset = 0;
+    
+    @FXML
+    private void onMousePressed(final MouseEvent event) {
+
+        xOffset = event.getSceneX();
+        yOffset = event.getSceneY();
+    }
+   
+    @FXML
+    private void onMouseDragged(final MouseEvent event) {
+        // TODO Auto-generated method stub
+        GUIState.getStage().setX(event.getScreenX() - xOffset);
+        GUIState.getStage().setY(event.getScreenY() - yOffset);
+
+    }
+}
--- part_1/src/main/java/example/HelloworldView.java
+++ part_1/src/main/java/example/HelloworldView.java
@@ -1,9 +1,14 @@
 package example;
 
+
 import de.felixroske.jfxsupport.AbstractFxmlView;
 import de.felixroske.jfxsupport.FXMLView;
+import de.felixroske.jfxsupport.GUIState;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 
 @FXMLView
 public class HelloworldView extends AbstractFxmlView {
+
 
 }
